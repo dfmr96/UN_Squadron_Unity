e2abe9f65ef22eb580cb44cb3a7cacbc04cd975d
David Marcano <dfmr96@gmail.com>
2023-04-21 10:08:12 -0300

    New Prefabs subfolders Vulkan cannon points added Vulkan POWs working Enemies can drop POWs

--- UN_Squadron_Clone/Assets/Scripts/Enemy.cs
+++ UN_Squadron_Clone/Assets/Scripts/Enemy.cs
@@ -3,28 +3,31 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] int health;
-    [SerializeField] GameObject explosionPrefab;
-    [SerializeField] GameObject bulletPrefab;
-    [SerializeField] float fireRate;
-    [SerializeField] float fireRateCounter;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private int _health;
+    [SerializeField] private GameObject _explosionPrefab;
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private float _fireRate;
+    [SerializeField] private float _fireRateCounter;
+    [SerializeField] private bool _canDropPOW;
+    [SerializeField] private VulkanPOWType _type;
+    [SerializeField] private GameObject[] _vulkanPOWs;
 
 
     private void Start()
     {
         //Cuando se instancie cualquier objeto con este Script va a saber cual es el jugador
-        player = FindObjectOfType<PlayerController>().gameObject;
+        _player = FindObjectOfType<PlayerController>().gameObject;
     }
 
     private void Update()
     {
         //fireRateCounter va sumando segundos
-        fireRateCounter += Time.deltaTime;
+        _fireRateCounter += Time.deltaTime;
 
         //Si los segundos transcurridos son mayores a 1 sobre fireRate...
         //1 / fireRate para poder definir cuantas balas por segundo se dispara
-        if (fireRateCounter > 1 / fireRate)
+        if (_fireRateCounter > 1 / _fireRate)
         {
             FireBullet();
         }
@@ -33,13 +36,27 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         //el dano estipulado en el parametro se le resta a la vida actual
-        health -= damage;
+        _health -= damage;
 
-        if (health <= 0)
+        if (_health <= 0)
         {
+            if (_canDropPOW)
+            {
+                switch (_type)
+                {
+                    case VulkanPOWType.Orange:
+                        Instantiate(_vulkanPOWs[0], transform.position, Quaternion.identity);
+                        break;
+                    case VulkanPOWType.Blue:
+                        Instantiate(_vulkanPOWs[1], transform.position, Quaternion.identity);
+                        break;
+                    default:
+                        break;
+                }
+            }
             //Si el enemigo muere porque su vida es 0 o menor...
             //...instancia el objeto explosion
-            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
             //...se destruye el objeto que porta este script para ahorrar memoria
             Destroy(gameObject);
         }
@@ -48,12 +65,12 @@ public class Enemy : MonoBehaviour
     public void FireBullet()
     {
         //...aimDir es la direccion a donde esta el jugador
-        Vector3 aimDir = (player.transform.position - transform.position).normalized;
+        Vector3 aimDir = (_player.transform.position - transform.position).normalized;
         //se crea la variable bullet para tener referencia del objeto recien creado
-        var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        var bullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
         //se accede a EnemyBullet y se setea la direccion a la que se va a mover con el metodo SetDirection
         bullet.GetComponent<EnemyBullet>().SetDirection(aimDir);
         //Se resetea fireRateCounter para aplicar cooldown
-        fireRateCounter = 0;
+        _fireRateCounter = 0;
     }
 }
--- UN_Squadron_Clone/Assets/Scripts/PlayerController.cs
+++ UN_Squadron_Clone/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Stats")]
+    [Space(10)]
     [SerializeField] float _speed = 15;
+    [SerializeField] float _vulkanFireRate;
+    [SerializeField] float _vulkanCounter;
+    [SerializeField] int _currentVulkan;
+    [SerializeField] int _pointsToNextVulkan;
+    [SerializeField] int _nextVulkanPoints;
+    [SerializeField] int _currentVulkanLevel;
+    [SerializeField] int[] _vulkanLevels = { 0, 2, 9, 18 };
+    [Space(20)]
+    [Header("GameObjects")]
+    [Space(10)]
     [SerializeField] GameObject _camera;
-    [SerializeField] float _horizontal;
-    [SerializeField] float _vertical;
-
-
     [SerializeField] GameObject _vulkanBullet;
     [SerializeField] GameObject _vulkanCannon;
-    [SerializeField] float _vulkanFireRate;
-    [SerializeField] float vulkanCounter;
+
+    //Privados
+    float _horizontal;
+    float _vertical;
     private BoxCollider2D _cameraCol;
     private BoxCollider2D _playerCol;
     private void Start()
@@ -26,9 +36,11 @@ public class PlayerController : MonoBehaviour
         _playerCol = GetComponent<BoxCollider2D>();
         //El jugador se vuelve hijo de la camara para que la pueda seguir
         transform.parent = _camera.transform;
+
+        _nextVulkanPoints = _vulkanLevels[_currentVulkanLevel + 1] - _currentVulkan;
     }
 
-    
+
     private void Update()
     {
         Movement();
@@ -39,15 +51,15 @@ public class PlayerController : MonoBehaviour
     private void FireVulkan()
     {
         //Contador de la ametralladora
-        vulkanCounter += Time.deltaTime;
+        _vulkanCounter += Time.deltaTime;
 
         //Si Espacio esta apretado y el contador es mayor a 1 sobre el firerate...
-        if (Input.GetKey(KeyCode.Space) && vulkanCounter > 1 / _vulkanFireRate)
+        if (Input.GetKey(KeyCode.Space) && _vulkanCounter > 1 / _vulkanFireRate)
         {
             //Se crea la bala en la posicion del objeto vulkanCannon
             Instantiate(_vulkanBullet, _vulkanCannon.transform.position, Quaternion.identity);
             //Se reinicia el contador para aplicar cooldown
-            vulkanCounter = 0;
+            _vulkanCounter = 0;
         }
     }
 
@@ -88,5 +100,29 @@ public class PlayerController : MonoBehaviour
         {
             _vertical = 0;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<VulkanPOW>() != null)
+        {
+            _currentVulkan += collision.GetComponent<VulkanPOW>().IncreaseVulkanPOWPoints();
+            CheckVulkanPoints();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void CheckVulkanPoints()
+    {
+        int pointsExceed = 0;
+        if (_currentVulkan > _nextVulkanPoints)
+        {
+            pointsExceed = _currentVulkan - _nextVulkanPoints;
+            _currentVulkan = _nextVulkanPoints;
+        }
+        if (_currentVulkan == _nextVulkanPoints) _currentVulkanLevel++;
+        _currentVulkan += pointsExceed;
+        _nextVulkanPoints = _vulkanLevels[_currentVulkanLevel + 1];
+        _pointsToNextVulkan = _nextVulkanPoints - _currentVulkan;
     }
 }
