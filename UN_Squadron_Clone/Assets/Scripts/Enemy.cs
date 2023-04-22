0a5da1544b9956c13bf150e80fbf73b821549295
David Marcano <dfmr96@gmail.com>
2023-04-22 14:12:39 -0300

    Turrent and Tank Animation Added

--- UN_Squadron_Clone/Assets/Scripts/Enemy.cs
+++ UN_Squadron_Clone/Assets/Scripts/Enemy.cs
@@ -1,4 +1,16 @@
+using System;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.Audio;
+
+public enum EnemyType
+{
+    helo,
+    turret,
+    bigTurret,
+    tank,
+    boss
+}
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class Enemy : MonoBehaviour
@@ -12,31 +24,37 @@ public class Enemy : MonoBehaviour
     [SerializeField] private bool _canDropPOW;
     [SerializeField] private VulkanPOWType _type;
     [SerializeField] private GameObject[] _vulkanPOWs;
-
+    [SerializeField] private EnemyType _enemyType;
+    [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
 
     private void Start()
     {
-        //Cuando se instancie cualquier objeto con este Script va a saber cual es el jugador
         _player = FindObjectOfType<PlayerController>().gameObject;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
         DeactiveAllComponents();
     }
 
     private void Update()
     {
-        //fireRateCounter va sumando segundos
         _fireRateCounter += Time.deltaTime;
 
-        //Si los segundos transcurridos son mayores a 1 sobre fireRate...
-        //1 / fireRate para poder definir cuantas balas por segundo se dispara
+        Vector3 aimDir = (_player.transform.position - transform.position).normalized;
+        if (_enemyType == EnemyType.turret) ChangeTurretSprite(aimDir);
+        if (_enemyType == EnemyType.tank)
+        {
+            MoveTank(aimDir);
+            ChangeTankSprite(aimDir);
+        }
         if (_fireRateCounter > 1 / _fireRate)
         {
-            FireBullet();
+            FireBullet(aimDir);
         }
     }
-    //Este metodo recibe como parametro el dano que se quiera aplicar al objeto que lo porta
     public void TakeDamage(int damage)
     {
-        //el dano estipulado en el parametro se le resta a la vida actual
         _health -= damage;
 
         if (_health <= 0)
@@ -55,23 +73,15 @@ public class Enemy : MonoBehaviour
                         break;
                 }
             }
-            //Si el enemigo muere porque su vida es 0 o menor...
-            //...instancia el objeto explosion
             Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
-            //...se destruye el objeto que porta este script para ahorrar memoria
             Destroy(gameObject);
         }
     }
 
-    public void FireBullet()
+    public void FireBullet(Vector3 dir)
     {
-        //...aimDir es la direccion a donde esta el jugador
-        Vector3 aimDir = (_player.transform.position - transform.position).normalized;
-        //se crea la variable bullet para tener referencia del objeto recien creado
         var bullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
-        //se accede a EnemyBullet y se setea la direccion a la que se va a mover con el metodo SetDirection
-        bullet.GetComponent<EnemyBullet>().SetDirection(aimDir);
-        //Se resetea fireRateCounter para aplicar cooldown
+        bullet.GetComponent<EnemyBullet>().SetDirection(dir);
         _fireRateCounter = 0;
     }
 
@@ -85,5 +95,103 @@ public class Enemy : MonoBehaviour
             comp.enabled = false;
         }
         GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    public void ChangeTurretSprite(Vector3 dir)
+    {
+        float angle = (int)Vector3.Angle(dir, -transform.right);
+        _spriteRenderer.flipX = false;
+        Debug.Log(angle);
+        if (angle >= 0 && angle < 20)
+        {
+            _spriteRenderer.sprite = _sprites[0];
+        }
+        else if (angle >= 20 && angle < 40)
+        {
+            _spriteRenderer.sprite = _sprites[1];
+        }
+        else if (angle >= 40 && angle < 60)
+        {
+            _spriteRenderer.sprite = _sprites[2];
+        }
+        else if (angle >= 60 && angle < 80)
+        {
+            _spriteRenderer.sprite = _sprites[3];
+        }
+        else if (angle >= 80 && angle < 100)
+        {
+            _spriteRenderer.sprite = _sprites[4];
+        }
+        else if (angle >= 100 && angle < 120)
+        {
+            _spriteRenderer.sprite = _sprites[3];
+            _spriteRenderer.flipX = true;
+        }
+        else if (angle >= 120 && angle < 140)
+        {
+            _spriteRenderer.sprite = _sprites[2];
+            _spriteRenderer.flipX = true;
+        }
+        else if (angle >= 140 && angle < 160)
+        {
+            _spriteRenderer.sprite = _sprites[1];
+            _spriteRenderer.flipX = true;
+        }
+        else if (angle >= 160 && angle < 180)
+        {
+            _spriteRenderer.sprite = _sprites[0];
+            _spriteRenderer.flipX = true;
+        }
+
+        //int i = 20;
+        ////_spriteRenderer.sprite = _sprites[(int)Math.Round(Convert.ToDecimal(angle / i) * 10)];
+        //Debug.Log("Sprite #" + (int)Math.Round(Convert.ToDecimal((angle / i)/10) * 10));
+    }
+
+    public void MoveTank(Vector3 dir)
+    {
+        transform.Translate(new Vector3(dir.x,0, 0) * 4 * Time.deltaTime);
+    }
+
+    public void ChangeTankSprite(Vector3 dir)
+    {
+        float angle = (int)Vector3.Angle(dir, -transform.right);
+        Debug.Log(angle);
+        if (angle >= 0 && angle < 20)
+        {
+            _spriteRenderer.sprite = _sprites[0];
+        }
+        else if (angle >= 20 && angle < 40)
+        {
+            _spriteRenderer.sprite = _sprites[1];
+        }
+        else if (angle >= 40 && angle < 60)
+        {
+            _spriteRenderer.sprite = _sprites[2];
+        }
+        else if (angle >= 60 && angle < 80)
+        {
+            _spriteRenderer.sprite = _sprites[3];
+        }
+        else if (angle >= 80 && angle < 100)
+        {
+            _spriteRenderer.sprite = _sprites[4];
+        }
+        else if (angle >= 100 && angle < 120)
+        {
+            _spriteRenderer.sprite = _sprites[5];
+        }
+        else if (angle >= 120 && angle < 140)
+        {
+            _spriteRenderer.sprite = _sprites[6];
+        }
+        else if (angle >= 140 && angle < 160)
+        {
+            _spriteRenderer.sprite = _sprites[7];
+        }
+        else if (angle >= 160 && angle < 180)
+        {
+            _spriteRenderer.sprite = _sprites[8];
+        }
     }
 }
