21853811e5da2323889e995ad06c3fd2fe111480
David Marcano <dfmr96@gmail.com>
2023-07-03 01:51:29 -0300

    Bomb sounds Added to prefab

--- UN_Squadron_Clone/Assets/BombLogic.cs
+++ UN_Squadron_Clone/Assets/BombLogic.cs
@@ -10,23 +10,39 @@ public class BombLogic : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] int impulseForce;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] audioClip;
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce((Vector2.right + Vector2.down) * impulseForce, ForceMode2D.Impulse);
+        PlaySound(audioClip[0]);
     }
     private void Update()
     {
 //        transform.Translate(Vector3.down * Time.deltaTime * speed);
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Enemy>() != null)
         {
             collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
-            Destroy(gameObject);
+            DestroyBomb();
         }
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void DestroyBomb()
+    {
+        animator.SetBool("isDestroyed", true);
+        rb.Sleep();
+        GetComponent<Collider2D>().enabled = false;
+        PlaySound(audioClip[1]);
+        Destroy(gameObject,0.5f);
     }
 }
