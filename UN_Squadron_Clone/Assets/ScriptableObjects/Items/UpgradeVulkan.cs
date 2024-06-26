10d459701e86a34335929f0c7065aea5a9a20695
Dave Marcano <dfmr96@gmail.com>
2024-06-26 16:18:58 -0300

    Nuevo ItemBehavior: Mejorar vulkan

---
+++ UN_Squadron_Clone/Assets/ScriptableObjects/Items/UpgradeVulkan.cs
@@ -0,0 +1,16 @@
+﻿using Player;
+using UnityEngine;
+
+namespace ScriptableObjects.Items
+{
+    [CreateAssetMenu(menuName = "ItemsBehavior/Create UpgradeVulkan", fileName = "UpgradeVulkan", order = 0)]
+    public class UpgradeVulkan : ItemBehavior
+    {
+        public int points;
+        public override void Use(PlayerController playerController)
+        {
+            playerController.FrontVulkan.AddPoints(points);
+            AudioManager.instance.vulkanPOW.Play();
+        }
+    }
+}
